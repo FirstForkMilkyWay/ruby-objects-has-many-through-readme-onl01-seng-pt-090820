a10091ea70f76f76fe00a0a4a897ea4293a60046
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-10-06 13:52:31 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,20 @@
 class Waiter
 
+attr_accessor :name, :age 
+
+@@all = []
+ 
+ 
+  def initialize(name, yrs_experience)
+    @name = name 
+    @yrs = yrs_experience
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+
+
+
 end
